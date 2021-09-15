405743e22caa9a6347346fd0760117c72a692967
pmabiala <pmabiala@me.com>
2021-09-15 17:45:44 +0000

    Step 4.3 step fullfilled

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -49,31 +49,31 @@ app.get("/",(req,res)=>{
 
 //The endpoint for the webserver ending with /url/emotion
 app.get("/url/emotion", (req,res) => {
-    // //Extract the url passed from the client through the request object
-    // let urlToAnalyze = req.query.url
-    // const analyzeParams = 
-    //     {
-    //         "url": urlToAnalyze,
-    //         "features": {
-    //             "keywords": {
-    //                             "emotion": true,
-    //                             "limit": 1
-    //                         }
-    //         }
-    //     }
+    //Extract the url passed from the client through the request object
+     let urlToAnalyze = req.query.url
+     const analyzeParams = 
+         {
+             "url": urlToAnalyze,
+             "features": {
+                 "keywords": {
+                                 "emotion": true,
+                                 "limit": 1
+                             }
+             }
+         }
      
-    //  const naturalLanguageUnderstanding = getNLUInstance();
+      const naturalLanguageUnderstanding = getNLUInstance();
      
-    //  naturalLanguageUnderstanding.analyze(analyzeParams)
-    //  .then(analysisResults => {
-    //     //Print the JSON returned by NLU instance as a formatted string
-    //     console.log(JSON.stringify(analysisResults.result.keywords[0].emotion,null,2));
-    //     //Please refer to the image to see the order of retrieval
-    //     return res.send(analysisResults.result.keywords[0].emotion,null,2);
-    //  })
-    //  .catch(err => {
-    //  return res.send("Could not do desired operation "+err);
-    //  });
+      naturalLanguageUnderstanding.analyze(analyzeParams)
+      .then(analysisResults => {
+         //Print the JSON returned by NLU instance as a formatted string
+         console.log(JSON.stringify(analysisResults.result.keywords[0].emotion,null,2));
+         //Please refer to the image to see the order of retrieval
+         return res.send(analysisResults.result.keywords[0].emotion,null,2);
+      })
+      .catch(err => {
+      return res.send("Could not do desired operation "+err);
+      });
 });
 
 //The endpoint for the webserver ending with /url/sentiment
